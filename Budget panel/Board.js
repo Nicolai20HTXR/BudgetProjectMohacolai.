0ad92e7af73e470c2f1268ecaa35a3d3ab99b2c1
stenkaster <75428915+stenkaster@users.noreply.github.com>
2022-04-26 11:47:16 +0200

    Input af title og penge
    display penge

--- Budget panel/Board.js
+++ Budget panel/Board.js
@@ -1,78 +1,189 @@
 class Kasse {
     constructor(x, y, r) {
-        this.x = x
-        this.y = y
-        this.r = r
-        this.pengeInput = createInput('')
-        this.pengeInput.position(this.x-((this.r-this.pengeInput.width)/2), this.y+((this.r-this.pengeInput.height)/4))
-        this.pengeInput.size(100)
-        this.textInput = ""
-        
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.editable = false;
+        this.tempMoneyValue = 0;
+        this.pengeInput = createInput("").attribute("maxlength", 11);
+        this.pengeInput.size(100);
+        this.pengeInput.position(
+            this.x + (this.r * 2 - this.pengeInput.width) / 2,
+            this.y + (this.r - this.pengeInput.height) / 4
+        );
+        this.textInput = "";
+
+        this.pengeAmountInput = createInput("0").attribute("type", "number");
+        this.pengeAmountInput.size(75);
+        this.pengeAmountInput.position(
+            this.x + (this.r * 2 - this.pengeAmountInput.width) / 2,
+            this.y + (this.r - this.pengeAmountInput.height) / 1.2
+        );
+
+        this.editimage = editimagepreload;
+
+        this.moneyObjects = [];
     }
     draw() {
-        //rColor = 
-        //fill(rColor,gColor,bColor);
         rect(this.x, this.y, this.r * 2, this.r * 8, 25);
         //første kasse
         rect(this.x, this.y, this.r * 2, this.r, 25);
-        
-        
-        this.textw = text(this.textInput,this.x+this.r/2,this.y+this.r/2)
-        this.textw.textSize(20)
+
+        if (this.editable) {
+            image(
+                this.editimage,
+                this.x + this.r * 2 - 35,
+                this.y + 10,
+                25,
+                25
+            );
+            if (
+                mouseX < this.x + this.r * 2 - 35 + 25 &&
+                mouseX > this.x + this.r * 2 - 35 &&
+                mouseY < this.y + 25 &&
+                mouseY > this.y + 10 &&
+                mouseIsPressed
+            ) {
+                this.editable = false;
+                this.pengeInput.show();
+                this.textInput = "";
+            }
+        }
+
+        this.textw = text(
+            this.textInput,
+            this.x + this.r / 2,
+            this.y + this.r / 2
+        );
+        this.textw.textSize(20);
         this.pengeInput.changed(() => {
-            this.textInput = this.pengeInput.value()
-            this.pengeInput.remove()
-            print()
-        }) 
+            this.textInput = this.pengeInput.value();
+            this.pengeInput.hide();
+            this.editable = true;
+        });
+
+        this.pengeAmountInput.input(() => {
+            this.tempMoneyValue = this.pengeAmountInput.value();
+            this.moneyObjects = [];
+        });
+
+        if (this.tempMoneyValue >= 1000) {
+            this.tempMoneyValue -= 1000;
+            this.moneyObjects.push(
+                new money(
+                    this.x + (this.r * 2 - kr1000.width) / 2,
+                    this.y + this.r,
+                    kr1000
+                )
+            );
+        } else if (this.tempMoneyValue >= 500) {
+            this.tempMoneyValue -= 500;
+            this.moneyObjects.push(
+                new money(
+                    this.x + (this.r * 2 - kr500.width) / 2,
+                    this.y + this.r,
+                    kr500
+                )
+            );
+        } else if (this.tempMoneyValue >= 200) {
+            this.tempMoneyValue -= 200;
+            this.moneyObjects.push(
+                new money(
+                    this.x + (this.r * 2 - kr200.width) / 2,
+                    this.y + this.r,
+                    kr200
+                )
+            );
+        } else if (this.tempMoneyValue >= 100) {
+            this.tempMoneyValue -= 100;
+            this.moneyObjects.push(
+                new money(
+                    this.x + (this.r * 2 - kr100.width) / 2,
+                    this.y + this.r,
+                    kr100
+                )
+            );
+        } else if (this.tempMoneyValue >= 50) {
+            this.tempMoneyValue -= 50;
+            this.moneyObjects.push(
+                new money(
+                    this.x + (this.r * 2 - kr50.width) / 2,
+                    this.y + this.r,
+                    kr50
+                )
+            );
+        }
+        for (let i = 0; i < this.moneyObjects.length; i++) {
+            this.moneyObjects[i].y = this.y + i * 25 + this.r;
+            this.moneyObjects[i].draw();
+        }
+    }
+    editable() {
+        this.editable = true;
     }
 }
 
-class AdderBox{
+class AdderBox {
     constructor(x, y, r) {
-        this.x = x
-        this.y = y
-        this.r = r
-        this.beingPressed = false
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.beingPressed = false;
     }
     draw() {
-        square(this.x, this.y, this.r, this.r / 4)
-        push()
-        strokeWeight(this.r / 16)
-        line(this.x + this.r / 2, this.y + this.r / 6, this.x + this.r / 2, this.y + this.r - this.r / 6)
-        line(this.x + this.r / 6, this.y + this.r / 2, this.x + this.r - this.r / 6, this.y + this.r / 2)
-        pop()
+        square(this.x, this.y, this.r, this.r / 4);
+        push();
+        strokeWeight(this.r / 16);
+        line(
+            this.x + this.r / 2,
+            this.y + this.r / 6,
+            this.x + this.r / 2,
+            this.y + this.r - this.r / 6
+        );
+        line(
+            this.x + this.r / 6,
+            this.y + this.r / 2,
+            this.x + this.r - this.r / 6,
+            this.y + this.r / 2
+        );
+        pop();
         if (this.x + this.r > width) {
-            resizeCanvas(width + 200, height)
+            resizeCanvas(width + 200, height);
         }
     }
     clickOn(array) {
-        if (mouseX > this.x && mouseX < this.x + this.r && mouseY > this.y && mouseY < this.y + this.r && mouseIsPressed && !this.beingPressed) {
-
-            this.mousePosColor = get(mouseX, mouseY)
-            this.boolPosColor = []
+        if (
+            mouseX > this.x &&
+            mouseX < this.x + this.r &&
+            mouseY > this.y &&
+            mouseY < this.y + this.r &&
+            mouseIsPressed &&
+            !this.beingPressed
+        ) {
+            this.mousePosColor = get(mouseX, mouseY);
+            this.boolPosColor = [];
 
             for (i = 0; i < this.mousePosColor.length - 1; i++) {
-                if (this.mousePosColor[i] == 255 || this.mousePosColor[i] == 0) {
+                if (
+                    this.mousePosColor[i] == 255 ||
+                    this.mousePosColor[i] == 0
+                ) {
                     this.boolPosColor[i] = true;
                 } else {
                     this.boolPosColor[i] = false;
                 }
             }
 
-            function boolSum(arr){
-                return arr.every(Boolean)
-            }
-                
-
-            if(boolSum(this.boolPosColor)){
-                this.x += 200
-                this.newKasse = new Kasse(this.x - 200, this.y, 100)
-                array.push(this.newKasse)
-
+            function boolSum(arr) {
+                return arr.every(Boolean);
             }
 
-
+            if (boolSum(this.boolPosColor)) {
+                this.x += 200;
+                this.newKasse = new Kasse(this.x - 200, this.y, 100);
+                array.push(this.newKasse);
+            }
         }
-        this.beingPressed = mouseIsPressed
+        this.beingPressed = mouseIsPressed;
     }
 }
--- Budget panel/main.js
+++ Budget panel/main.js
@@ -1,29 +1,31 @@
-function preload(){
+function preload() {
     kr50 = loadImage('50dkk.jpg');
     kr100 = loadImage('100dkk.jpg');
     kr200 = loadImage('200dkk.jpg');
     kr500 = loadImage('500dkk.jpg');
-    kr1000 = loadImage('1000dkk.jpg');   
+    kr1000 = loadImage('1000dkk.jpg');
+    editimagepreload = loadImage("Edit.png")
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     //dkkobj1000=new Kasse(25,25,100)
     objArray = []
-    x = new AdderBox(25,25,100)
+    x = new AdderBox(25, 25, 100)
+    //l = new money(25,25,kr1000)
 }
 
 function draw() {
     background(210);
-    //dkkobj1000.draw()
     x.draw()
     x.clickOn(objArray)
-    for(i = 0; i < objArray.length; i++){
+    for (i = 0; i < objArray.length; i++) {
         objArray[i].draw()
     }
     
-    
+    //l.draw()
+
 }
-function mousePressed(){
+function mousePressed() {
     //dkkobj1000.dragandDrop()
 }
