c792c934ec26547e1b47d915c6e473e63c54389a
Nicolai20HTXR <skole.nicolai.wiese@gmail.com>
2022-04-26 14:40:10 +0200

    Removing un-needed stuff on main
    old code, forgot to remove

--- Budget panel/main.js
+++ Budget panel/main.js
@@ -4,28 +4,20 @@ function preload() {
     kr200 = loadImage('200dkk.jpg');
     kr500 = loadImage('500dkk.jpg');
     kr1000 = loadImage('1000dkk.jpg');
-    editimagepreload = loadImage("Edit.png")
+    editimagepreload = loadImage("Edit.png");
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    //dkkobj1000=new Kasse(25,25,100)
-    objArray = []
-    x = new AdderBox(25, 25, 100)
-    //l = new money(25,25,kr1000)
+    objArray = [];
+    x = new AdderBox(25, 25, 100);
 }
 
 function draw() {
     background(210);
-    x.draw()
-    x.clickOn(objArray)
+    x.draw();
+    x.clickOn(objArray);
     for (i = 0; i < objArray.length; i++) {
-        objArray[i].draw()
+        objArray[i].draw();
     }
-    
-    //l.draw()
-
-}
-function mousePressed() {
-    //dkkobj1000.dragandDrop()
 }
